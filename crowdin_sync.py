6676ba6bba76d63aa6182e916b8017656c2eb1ea
Michael Bestas <mikeioannina@gmail.com>
2015-03-19 14:14:13 +0200

    crowdin_sync: Ignore files that don't match the project configuration
    * Avoid log spam due to unmatched files from different branches
    
    Change-Id: I588c61dea35aa3421b14318de85b67c988b70cf6

--- crowdin_sync.py
+++ crowdin_sync.py
@@ -166,11 +166,11 @@ if not args.no_download:
     print('\nSTEP 2: Download Crowdin translations')
     print('Downloading Crowdin translations (AOSP supported languages)')
     # Execute 'crowdin-cli download' and show output
-    run_command(['crowdin-cli', '--config=crowdin/crowdin_' + default_branch + '.yaml', '--identity=crowdin/config.yaml', 'download'])
+    run_command(['crowdin-cli', '--config=crowdin/crowdin_' + default_branch + '.yaml', '--identity=crowdin/config.yaml', 'download', '--ignore-match'])
 
     print('\nDownloading Crowdin translations (non-AOSP supported languages)')
     # Execute 'crowdin-cli download' and show output
-    run_command(['crowdin-cli', '--config=crowdin/crowdin_' + default_branch + '_aosp.yaml', '--identity=crowdin/config_aosp.yaml', 'download'])
+    run_command(['crowdin-cli', '--config=crowdin/crowdin_' + default_branch + '_aosp.yaml', '--identity=crowdin/config_aosp.yaml', 'download', '--ignore-match'])
 
     print('\nSTEP 3: Remove useless empty translations')
     # Some line of code that I found to find all XML files
