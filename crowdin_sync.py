86315aed4e4f274070437c0b83a8454815fb3a11
Michael W <baddaemon87@gmail.com>
2025-04-22 19:21:11 +0200

    crowdin: Improve project path determination
    * Instead of building the possible path manually, let's just compare
      all extracted file paths against the list of known project paths
    * Longest match wins
    * We determine all projects first and then push them
    
    Change-Id: I76178fdce8ec27248580d978991a6f0b840008c7

--- crowdin_sync.py
+++ crowdin_sync.py
@@ -6,7 +6,7 @@
 # directly to LineageOS' Gerrit.
 #
 # Copyright (C) 2014-2016 The CyanogenMod Project
-# Copyright (C) 2017-2022 The LineageOS Project
+# Copyright (C) 2017-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
